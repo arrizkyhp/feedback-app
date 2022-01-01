83eeabc4b0b4bcd800cf7f65cbf4129a33bbab40
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2022-01-01 23:34:02 +0700

    first 🔥(Section 3 | 14)

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+
+function Header({ title, bgColor, textColor }) {
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor,
+    }
+    return (
+        <header style={headerStyles}>
+            <div className="container">
+                <h1>{title}</h1>
+            </div>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95'
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+}
+export default Header
