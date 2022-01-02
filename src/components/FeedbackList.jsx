3ccc2c5bef88568cc604bdc3b325e3d8f8d4dd16
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2022-01-02 19:48:54 +0700

    Update (Section 4 | 24)

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,29 +1,54 @@
 import PropTypes from 'prop-types';
 import FeedbackItem from "./FeedbackItem"
-
+import {motion, AnimatePresence} from 'framer-motion'
 
 function FeedbackList({ feedback, handleDelete }) {
     if (!feedback || feedback.length === 0) { return <p>No Feedback yet..</p>};
 
     return (
-      <div>
+
         <div className="feedback-list">
+            <AnimatePresence>
           {feedback.map((item) => (
-              <FeedbackItem
+              <motion.div
                 key={item.id}
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                exit={{opacity: 0}}
+              >
+              <FeedbackItem
                 item={item}
                 handleDelete={handleDelete}
             />
+            </motion.div>
           ))}
+          </AnimatePresence>
         </div>
-      </div>
+
     );
+
+    // return (
+    //   <div>
+    //     <div className="feedback-list">
+    //       {feedback.map((item) => (
+    //           <FeedbackItem
+    //             key={item.id}
+    //             item={item}
+    //             handleDelete={handleDelete}
+    //         />
+    //       ))}
+    //     </div>
+    //   </div>
+    // );
 }
 
 FeedbackList.propTypes = {
     feedback: PropTypes.arrayOf(
         PropTypes.shape({
-            id: PropTypes.number.isRequired,
+            id: PropTypes.oneOfType([
+                PropTypes.string,
+                PropTypes.number,
+            ]).isRequired,
             rating: PropTypes.number.isRequired,
             text: PropTypes.string.isRequired
         })
---
+++ src/components/RatingSelect.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+function RatingSelect({ select }) {
+    const [number, setNumber] = useState(10);
+    const [selected, setSelected] = useState(10)
+
+    const rateNumber = []
+    for (let i = 0; i < number; i++) {
+        rateNumber.push(i+1);
+    }
+
+    const handleChange = (e) => {
+        // + change string to number
+        setSelected(+e.currentTarget.value);
+        select(+e.currentTarget.value);
+    }
+    return (
+        <ul className='rating'>
+            {rateNumber.map((item) => {
+                return <li key={item}>
+                    <input
+                        type='radio'
+                        id={`num${item}`}
+                        name='rating'
+                        value={item}
+                        onChange={handleChange}
+                        checked={selected === item}
+                    />
+                    <label htmlFor={`num${item}`}>{item}</label>
+                </li>
+            })}
+    </ul>
+    )
+}
+
+export default RatingSelect
